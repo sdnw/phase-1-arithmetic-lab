dfbabf33efdd972ee3886f7f4220c6825a9fbb1b
Steven <stevendawn98@gmail.com>
2022-06-06 16:56:24 -0400

    commit tree ad1535c49a17e68eafd77b1709717a14575e7e4f

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// Multiply
-const num1 = 31;
-const num2 = 2;
+// multiply
+const num1 = 31
+const num2 = 2
 
-const multiply = num1 * num2;
+const multiply = num1 * num2
 
-// Random integer greater than 0
-const random = Math.floor(Math.random() * 10) + 1
+// random
+let random = Math.floor(Math.random() * 10) + 1
 
-// Mod
-const num3 = 19
-const num4 = 5
+// mod
+let num3 = 19
+let num4 = 5
 
-const mod = num3 % num4;
+const mod = num3 % num4
 
-// Max
-const max = Math.max(1, 2, 20, 4)
+// max
+const max = Math.max(1, 2, 3, 20)
